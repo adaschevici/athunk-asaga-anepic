906f5dbb0974f8cb3b6ba04a20286322695fcc27
adaschevici <a.daschevici@gmail.com>
2018-03-31 23:08:43 +0100

    Fix team logo cut on team slide

--- src/presentation/slides/2.5/index.js
+++ src/presentation/slides/2.5/index.js
@@ -3,20 +3,14 @@ import { Slide, Image, Appear } from 'spectacle';
 
 import preloader from "spectacle/lib/utils/preloader";
 
+import style from './style.scss';
+
 const images = {
   birdieTeam: require('../../../assets/birdie-team.jpeg'),
   birdie: require('../../../assets/birdie.jpg'),
 };
 
 preloader(images);
-
-const avatarStyle = {
-  position: 'absolute',
-  marginTop: '30%',
-  marginLeft: '35%',
-  height: '60%',
-  borderRadius: "18.750em",
-};
 
 const notes = `I work in a startup where we aim to provide families with a system that allows
   them to have peace of mind about their loved ones as they grow old.`;
@@ -32,8 +26,7 @@ export default (
       <Appear>
         <Image
           src={images.birdie.replace("/", "")}
-          margin="0px auto 40px"
-          style={avatarStyle}
+          className={style.birdie}
         />
       </Appear>
     </div>
