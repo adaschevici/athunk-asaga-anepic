4ac10b193c3afdd6324bd0d0bc4fb0ee18ce5d41
adaschevici <a.daschevici@gmail.com>
2018-05-07 14:05:51 +0100

    Make handle more responsive

---
+++ src/presentation/utils/handle/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'spectacle';
+
+import styles from './style.scss';
+
+export default () => (
+  <Text className={styles.handle}>@_arturdas</Text>
+);
+
