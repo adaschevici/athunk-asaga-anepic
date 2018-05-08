ca7086422eb8a86b42d10cdb86bfcbb9ce4cc111
adaschevici <a.daschevici@gmail.com>
2018-05-08 07:38:43 +0100

    Make credist more responsive

--- src/presentation/utils/photo-credit/index.js
+++ src/presentation/utils/photo-credit/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Text } from 'spectacle';
 
+import styles from './style.scss';
+
 const handleClass = {
   position: 'absolute',
   top: '100%',
   left: '0%',
-  color: '#0892D0',
 };
 
 export default (props) => (
-  <Text style={handleClass}>{props.text}</Text>
+  <Text className={styles.photoCredit}>{props.text}</Text>
 );
 
 
